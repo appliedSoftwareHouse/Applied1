f24edd6f01e8c67003b44aa2827ce2d2d47173a2
Muhammad Aamir Jahangir <aamir_jk@hotmail.com>
2022-11-24 23:00:20 +0500

    HOME 24-NOV-2022

--- Applied_WebApplication/Pages/Accounts/COA.cshtml.cs
+++ Applied_WebApplication/Pages/Accounts/COA.cshtml.cs
@@ -5,8 +5,35 @@ namespace Applied_WebApplication.Pages.Accounts
 {
     public class COAModel : PageModel
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        private bool IsPosted = false;
+
         public void OnGet()
         {
+            if (!IsPosted) 
+            {
+                ID = 999;
+                Title = "My Name type here";
+            }
+            else
+            {
+                ID = ID;
+                Title =  Title;
+
+            }
+            
+
+            
         }
+
+        public void OnPost()
+        {
+            IsPosted = true;
+            ID = ID+1;
+
+        }
+
     }
 }
