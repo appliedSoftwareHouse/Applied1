b8b37e3de53bb8e7e45cdee5029a2ffa987c898f
Muhammad Aamir Jahangir <aamir_jk@hotmail.com>
2022-11-24 17:25:32 +0500

    Home 24-11-2022

---
+++ Applied_WebApplication/Pages/Accounts/COA.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Applied_WebApplication.Pages.Accounts
+{
+    public class COAModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
