1d7fd68a08eb614f4f87c8ab9b3763d003cd29a9
Muhammad Aamir Jahangir <aamir_jk@hotmail.com>
2022-11-28 22:06:02 +0500

    HOME 28-NOV-2022 10:00 PM

--- Applied_WebApplication/Pages/Accounts/COA_Edit.cshtml.cs
+++ Applied_WebApplication/Pages/Accounts/COA_Edit.cshtml.cs
@@ -1,4 +1,5 @@
 using Applied_WebApplication.Data;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.CodeAnalysis.CSharp;
@@ -13,10 +14,12 @@ namespace Applied_WebApplication.Pages.Accounts
     public class COA_EditModel : PageModel
     {
         public DataTableClass COA;
-        public double MyID;
-        public string MyTitle;
+        public int ID,Nature,Class,Notes;
+        public string Title;
+        public decimal OBal;
 
         public async Task<IActionResult> OnGetAsync(int? id)
+        //public IActionResult OnGet(int? id)
         {
             if (id == null)
             {
@@ -24,14 +27,54 @@ namespace Applied_WebApplication.Pages.Accounts
             }
 
             COA = new DataTableClass(Tables.COA.ToString(),  Convert.ToDouble(id));
-            
-            MyID = (double)COA.CurrentRow["ID"];
-            MyTitle = COA.CurrentRow["Title"].ToString();
 
+            ID = (int)COA.CurrentRow["ID"];
+            Title = (string)COA.CurrentRow["Title"];
+            Nature = (int)COA.CurrentRow["Nature"];
+            Class = (int)COA.CurrentRow["Class"];
+            Notes = (int)COA.CurrentRow["Notes"];
             return Page();
         }
 
+        public IActionResult OnPostSubmit(Record _Record)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                string resultTitle = Title;
+
+
+                // do something
+                return RedirectToPage("COA");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public class Record
+        {
+            [BindProperty]
+            int ID { get; set; }
+
+            [BindProperty]
+            string Title { get; set; }
+
+            int Nature { get; set; }
+
+            int Class { get; set; }
+
+            int Notes { get; set; }
+
+            decimal OBal { get; set; }
+
+        }
+
         
+        
+
+       
 
     }
 }
