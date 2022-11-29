e7e3cea5eb6f8e1474316195c3778afeada6ef0c
Muhammad Aamir Jahangir <aamir_jk@hotmail.com>
2022-11-29 18:24:41 +0500

    WINMARK 29-11-2022

--- Applied_WebApplication/Pages/Accounts/COA_Edit.cshtml.cs
+++ Applied_WebApplication/Pages/Accounts/COA_Edit.cshtml.cs
@@ -13,35 +13,35 @@ namespace Applied_WebApplication.Pages.Accounts
 {
     public class COA_EditModel : PageModel
     {
-        public DataTableClass COA;
-        public int ID,Nature,Class,Notes;
-        public string Title;
-        public decimal OBal;
+        public DataTableClass COA; 
+        public Record _Record { get; set; } = new Record();
 
         public async Task<IActionResult> OnGetAsync(int? id)
-        //public IActionResult OnGet(int? id)
+
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            COA = new DataTableClass(Tables.COA.ToString(),  Convert.ToDouble(id));
+            COA = new DataTableClass(Tables.COA.ToString(), Convert.ToDouble(id));
 
-            ID = (int)COA.CurrentRow["ID"];
-            Title = (string)COA.CurrentRow["Title"];
-            Nature = (int)COA.CurrentRow["Nature"];
-            Class = (int)COA.CurrentRow["Class"];
-            Notes = (int)COA.CurrentRow["Notes"];
+            _Record.ID = (int)COA.CurrentRow["ID"];
+            _Record.Title = (string)COA.CurrentRow["Title"];
+            _Record.Nature = (int)COA.CurrentRow["Nature"];
+            _Record.Class = (int)COA.CurrentRow["Class"];
+            _Record.Notes = (int)COA.CurrentRow["Notes"];
             return Page();
         }
 
-        public IActionResult OnPostSubmit(Record _Record)
+        public async Task<IActionResult> OnPostSubmit(Record? _FillRecord)
         {
             if (ModelState.IsValid)
             {
-                
-                string resultTitle = Title;
+
+                string exisitTitle = (string)COA.CurrentRow["Title"];
+
+                string resultTitle = _FillRecord.Title;
 
 
                 // do something
@@ -56,25 +56,19 @@ namespace Applied_WebApplication.Pages.Accounts
         public class Record
         {
             [BindProperty]
-            int ID { get; set; }
+            public int ID { get; set; }
 
             [BindProperty]
-            string Title { get; set; }
+            public string Title { get; set; }
 
-            int Nature { get; set; }
+            public int Nature { get; set; }
 
-            int Class { get; set; }
+            public int Class { get; set; }
 
-            int Notes { get; set; }
+            public int Notes { get; set; }
 
-            decimal OBal { get; set; }
+            public decimal OBal { get; set; }
 
         }
-
-        
-        
-
-       
-
     }
 }
