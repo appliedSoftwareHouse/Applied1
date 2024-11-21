ddbb55b3634bcdd3f9ec0e8b730bbee0d63fbc01
appliedsoftwarehouse <info@jahangir.com>
2024-11-21 17:57:15 +0300

    HPLT - 21-NOV-2024 Customer Add filter OK

--- Applied_WebApplication/Pages/Sales/Customers.cshtml.cs
+++ Applied_WebApplication/Pages/Sales/Customers.cshtml.cs
@@ -1,13 +1,52 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System.Drawing;
 
 namespace Applied_WebApplication.Pages.Sales
 {
     [Authorize]
     public class CustomersModel : PageModel
     {
+        [BindProperty] public string SearchText { set; get; } 
+        internal DataTable Customer { get; set; }
+        internal DataTable MyTable { get; set; }
+
+        public string UserName => User.Identity.Name;
         public void OnGet()
         {
+            SearchText = AppRegistry.GetText(UserName,"ctmSearch");
+            Customer = DataTableClass.GetTable(UserName, Tables.Customers, $"{GetFilter()}");
+            
+        }
+
+        private string GetFilter()
+        {
+            var _Filter = string.Empty;
+
+            _Filter += $"[Title] Like '%{SearchText}%' OR ";
+            _Filter += $"[City] Like '%{SearchText}%' OR ";
+            _Filter += $"[Phone] Like '%{SearchText}%' OR ";
+            _Filter += $"[Mobile] Like '%{SearchText}%' OR ";
+            _Filter += $"[Email] Like '%{SearchText}%'";
+
+            return _Filter;
+        }
+
+        public IActionResult OnPostRefresh()
+        {
+            AppRegistry.SetKey(UserName, "ctmSearch", SearchText, KeyType.Text);
+            //Customer = DataTableClass.GetTable(UserName, Tables.Customers, $"{GetFilter()}");
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostClear()
+        {
+            
+            AppRegistry.SetKey(UserName, "ctmSearch", string.Empty, KeyType.Text);
+            //Customer = DataTableClass.GetTable(UserName, Tables.Customers, $"{GetFilter()}");
+            return RedirectToPage();
         }
     }
 }
