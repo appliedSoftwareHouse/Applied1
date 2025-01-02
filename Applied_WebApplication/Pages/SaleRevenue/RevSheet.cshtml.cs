f864b45218f4004f68d4cca721178a69d18e8875
appliedsoftwarehouse <info@jahangir.com>
2025-01-02 08:19:26 +0500

    HPLT - 02-JAN-2025. Cash book bug removed

--- Applied_WebApplication/Pages/SaleRevenue/RevSheet.cshtml.cs
+++ Applied_WebApplication/Pages/SaleRevenue/RevSheet.cshtml.cs
@@ -42,11 +42,10 @@ namespace Applied_WebApplication.Pages.SaleRevenue
             var _Date1 = AppFunctions.MinDate(Variables.MinDate, Variables.MaxDate).ToString(DateYMD);
             var _Date2 = AppFunctions.MaxDate(Variables.MinDate, Variables.MaxDate).ToString(DateYMD);
 
-            var _Filter = $"COA={id}";
             var _Dates = new string[] { _Date1, _Date2 };
             var _Book = "Cash";
 
-            Cashbook = DataTableClass.GetTable(UserName, SQLQuery.BookLedger(_Filter, _Dates, _Book));
+            Cashbook = DataTableClass.GetTable(UserName, SQLQuery.BookLedger((int)id, _Dates, _Book));
 
             if (Variables.IsPosted1 == 1)                        // Not posted.
             {
