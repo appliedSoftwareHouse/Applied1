6883959eb824d7dafe2840c5588546819b7eceb3
appliedsoftwarehouse <info@jahangir.com>
2025-02-15 23:12:45 +0300

    HPLT - 15-Feb-2025. Sale Invoice 2 HTML Table Working.

--- Applied_WebApplication/Pages/Sales/SaleInvoice2.cshtml.cs
+++ Applied_WebApplication/Pages/Sales/SaleInvoice2.cshtml.cs
@@ -4,7 +4,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Applied_WebApplication.Pages.Sales
 {
-    public class SaleInvoice2Model : PageModel
+    public interface ISaleInvoice2
+    {
+
+    }
+
+
+    public class SaleInvoice2Model : PageModel, ISaleInvoice2
     {
         [BindProperty] public Parameters myModel { get; set; }
         public string UserName => User.Identity.Name;
@@ -18,6 +24,8 @@ namespace Applied_WebApplication.Pages.Sales
         public void OnGet()
         {
             myModel = new();
+            myModel.fields = NewRecord();
+
             CompanyList = AppFunctions.GetList(UserName, Tables.Customers, "");
             EmployeeList = AppFunctions.GetList(UserName, Tables.Employees, "");
             StockList = AppFunctions.GetList(UserName, Tables.Inventory, "");
@@ -25,34 +33,84 @@ namespace Applied_WebApplication.Pages.Sales
             TaxesList = AppFunctions.GetList(UserName, Tables.Taxes, "");
         }
 
-        public IActionResult OnPostSave()
+        private DataFields NewRecord()
         {
-            
+            myModel = new();
+            DataFields _fields = new();
+            {
+                _fields.Vou_No = "New";
+                _fields.ID1 = 0;
+                _fields.ID2 = 0;
+                _fields.Sr_No = 1;
 
-            myModel.Save_Voucher();
-            return Page();
-            
+                _fields.Vou_Date = AppRegistry.GetDate(UserName, "invVouDate");
+                _fields.Inv_Date = _fields.Vou_Date;
+                _fields.Pay_Date = _fields.Vou_Date.AddDays(30);
+            }
+
+            myModel.Add(_fields);
+            myModel.fields = myModel.FirstRecord;
+
+            return _fields;
         }
 
-        
+        private DataFields AddRecord()
+        {
+            DataFields _fields = new();
+
+            if (myModel.FirstRecord != null || myModel.fieldsList.Count > 0)
+            {
+                _fields = myModel.FirstRecord;
+                _fields.Sr_No = myModel.fieldsList.Max(x => x.Sr_No) + 1;
+                _fields.Inventory = 0;
+                _fields.Qty = 0;
+                _fields.Rate = 0;
+                _fields.Amount = 0;
+                _fields.Tax = 0;
+                _fields.TaxRate = 0;
+                _fields.TaxAmount = 0;
+                _fields.NetAmount = 0;
+                _fields.Batch = "";
+                _fields.Project = 0;
+                _fields.Description = "";
+            }
+
+            myModel.fieldsList.Add(_fields);
+            return _fields;
+        }
+
+        public IActionResult OnPostSave()
+        {
+            myModel.Save_Voucher();
+            return Page();
+        }
     }
 
+    
 
-    public class Parameters
+
+    public class Parameters 
     {
+        public Parameters()
+        {
+        }
+
         public DataFields fields { get; set; } = new();
         public List<DataFields> fieldsList { get; set; } = new();
-        
+        public DataFields? FirstRecord => fieldsList.Any() ? fieldsList.FirstOrDefault() : null;
+        public int MaxSrNo => fieldsList.Any() ? fieldsList.Max(x => x.Sr_No) : 0;
+
+
         public int Count => fieldsList?.Count ?? 0;
 
         public void Add(DataFields _fields)
         {
-            fieldsList.Add(fields);
+            fieldsList.Add(_fields);
         }
 
         public void Remove(DataFields _fields)
         {
-            fieldsList.Remove(fields);
+            fieldsList.Remove(_fields);
         }
 
         public bool Save_Voucher()
@@ -95,6 +153,18 @@ namespace Applied_WebApplication.Pages.Sales
         [Required] public int TaxRate { get; set; }
         [Required] public decimal TaxAmount { get; set; }
         [Required] public decimal NetAmount { get; set; }
+
+        public string TitleInventory { get; set; } = string.Empty;
+        public string TitleTax { get; set; } = string.Empty;
+
+
+        public string NumFormat2 = AppRegistry.Currency2d;
+        public string TQty => Qty.ToString(NumFormat2);
+        public string TRate => Rate.ToString(NumFormat2);
+        public string TAmount => Amount.ToString(NumFormat2);
+        public string TNetAmount => NetAmount.ToString(NumFormat2);
+        public string TTaxAmount => TaxAmount.ToString(NumFormat2);
+        public string TTaxRate => TaxRate.ToString(NumFormat2);
 
     }
 }
