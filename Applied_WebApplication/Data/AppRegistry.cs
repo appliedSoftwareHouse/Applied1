d89e43417ee5fb53a6e79623e4ab7ecf120f29b0
Mohammed Amir Jahangir <m.amir@modtechno.com>
2026-07-02 14:22:25 +0300

    MTS - 02-JUN-2026. Update AppRegistry Key Discal Year from 22 to year 30

--- Applied_WebApplication/Data/AppRegistry.cs
+++ Applied_WebApplication/Data/AppRegistry.cs
@@ -50,7 +50,7 @@ namespace Applied_WebApplication.Data
         public static DateTime GetFiscalFrom()
         { return new DateTime(2022, 07, 01); }           // In future addign value from App Registry
         public static DateTime GetFiscalTo()
-        { return new DateTime(2026, 06, 30); }
+        { return new DateTime(2030, 06, 30); }
 
         public static DateTime GetFiscalFrom(string UserName)
         {
